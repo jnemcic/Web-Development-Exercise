a9290dd266195d3a50c197e0b258e2103d16b69e
Ivan Weiler <ivan.weiler@inchoo.net>
2016-04-05 14:13:27 +0200

    Changes.

--- js/app.js
+++ js/app.js
@@ -15,28 +15,50 @@ $("#gallery").append($overlay);
 
 var $slides = $("a.lightbox");
 
-$slides.click(function(event){
-  event.preventDefault(); 
-  var imageLocation = $(this).attr("href");
-  $image.attr("src", imageLocation);
 
-  $overlay.show();
-  var captionText = $(this).children("img").attr("alt");
-  $caption.text(captionText);
+var current;
 
-    $next.click(function() {
-        $slides.each(function(index,element) {
-          imageLocation = element.href;
+$slides.each(function(i, el) {
 
-          console.log( index + ": " + $( this).attr("href") );
-          $image.attr("src", imageLocation);
-          captionText = $(this).children("img").attr("alt");
-          $caption.text(captionText);
-        });
+  $(el).click(function(event){
+    event.preventDefault();
+
+    current = i;
+    console.log(current);
+
+    var imageLocation = $(this).attr("href");
+    $image.attr("src", imageLocation);
+
+    var captionText = $(this).children("img").attr("alt");
+    $caption.text(captionText);
+
+    $overlay.show();
   });
+
 });
 
-//test
+
+$next.click(function(event) {
+  event.preventDefault();
+
+  //var next = current + 1;
+  current = current + 1;
+  var nextLightbox = $($slides[current]);
+  //console.log(nextLightbox);
+
+  var imageLocation = nextLightbox.attr("href");
+  $image.attr("src", imageLocation);
+
+  var captionText = nextLightbox.children("img").attr("alt");
+  $caption.text(captionText);
+
+});
+
+
+$exit.click(function() {
+  $overlay.hide();
+});
+
 
 /*
 $slides.each(function(index, element) {
@@ -80,7 +102,3 @@ $slides.each(function(index, element) {
 //});
 
 
-
-$exit.click(function(){
-  $overlay.hide();
-});
